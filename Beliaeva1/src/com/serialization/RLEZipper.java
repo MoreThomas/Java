46c9d561fad2c14a54250315bcc427760d18fade
scorpioa <andrey.ashomok@gmail.com>
2013-11-19 12:16:24 +0200

    resolve RLE compressing in chapter 'Serialization'

--- Beliaeva1/src/com/serialization/RLEZipper.java
+++ Beliaeva1/src/com/serialization/RLEZipper.java
@@ -3,20 +3,22 @@ package com.serialization;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.ArrayList;
 
-// doesn't work yet
-// check this
 // http://www.oracle.com/technetwork/articles/java/compress-1565076.html
-
+/*
+ * Программа считывает входной текстовый файл из args[0] (на данный момент это eula),
+ * если символы повторяются в входном файле, то программа их счетает и записывает в буффер
+ * как КОЛ-ВОхСИМВОЛ (типа 11ха), если не повторяется, то просто пишет символ.
+ * Когда все прочитано, буффер пишется в выходной файл, к-рый задан в args[1] (на данный момент это zipped).
+ */
 
 public class RLEZipper implements Serializable {
+	private static final long serialVersionUID = -3154548929368036079L;
+
 	public static void main(String[] args) throws IOException {
 		File in = new File(args[0]);
 		File out = new File(args[1]);
@@ -25,44 +27,42 @@ public class RLEZipper implements Serializable {
 		StringBuffer buffString = new StringBuffer();
 		
 		while(buffReader.ready()) {
-			String str = buffReader.readLine();
+			String str = convertToRLE(buffReader.readLine());
 			buffString.append(str);
-			System.out.println(str);
 		}
 		
-		buffReader.close();
+		buffReader.close();				
 		
-		convertToRLE(buffString.toString());
+		BufferedWriter buffWriter = new BufferedWriter(new FileWriter(out));		
+		buffWriter.write(buffString.toString());
 		
-//		BufferedWriter buffWriter = new BufferedWriter(new FileWriter(out));
-//							
-//		
-//		
-//		
-//		buffWriter.close();
+		buffWriter.close();
 	}
 	
-	public static void convertToRLE(String str) {
-		ArrayList<String> list = new ArrayList<String>();
-		char[] chArray = str.toCharArray();
-		char ch='0';
-		int count = 1;
+	public static String convertToRLE(String str) {
+		StringBuffer buff = new StringBuffer();		
+		char[] ch = str.toCharArray();				
+		int count = 1;		
 		
-		for (int i = 1; i < chArray.length; i++) {
-			if(chArray[i-1] == chArray[i] && chArray[i] != ' ') {
+		for (int i = 1; i < ch.length; i++) {
+			char c = ch[i-1];
+		
+			if(ch[i] == ' ')
+				continue;
+			
+			if(c == ch[i]) {
 				count++;
-				ch = chArray[i];
+				continue;
 			} else {
-				if(count>1) {
-					list.add(count+"x"+String.valueOf(ch));
-				}
-				count = 1;
+				if(count>1) 
+					buff.append(count+"x"+String.valueOf(c));
+				else 
+					buff.append(String.valueOf(c));
+				
+				count = 1;				
 			}
 		}
-		
-		
-		for (String s : list) {
-			System.out.println(s);
-		}
+				
+		return buff.toString();
 	}
 }
